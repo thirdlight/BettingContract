0a29b8ece71194331df7df53e0fb354513e2ba63
Nicole Grizzle <thirdlight@users.noreply.github.com>
2019-04-21 14:00:34 -0400

    Update Betting.sol
    made contract more secure

--- contracts/Betting.sol
+++ contracts/Betting.sol
@@ -6,14 +6,15 @@ contract Betting {
         uint256 amountBet;
         address payable gambler;
     }
-    
-    mapping(bytes32 => Bets) public bets;
+    mapping(bytes32 => Bets) public bets; // holds information associated with betIds
     bytes32[] betIds = new bytes32[](10); // holds all betIds
     uint256 poolAmount; // functionally identical to the contract's balance.
     uint256 startTime; // time that betting rounds starts
     uint256 endTime; // time it ends
     uint256 minimumBet; // minimum buyin
+    uint256 winningColor; // holds winning color
     bool running; // whether or not there is a round currently ongoing
+    address payable[]  players = new address payable[](10); // used to limit the amount of players (gas costs)
 
     
     event TimeOut(uint256 current_time); // have a listener that watches this event on javascript-side
@@ -21,26 +22,24 @@ contract Betting {
     
     constructor(uint256 _minimumBet) public {
         running = false; // shouldn't be started automatically
-        minimumBet = _minimumBet; 
+        minimumBet = _minimumBet;  // sets minimum bet
         betIds.length = 0; // zero betIds at start
     }
     
     function startBet(uint256 _endTime) public {
         // holds the start/end times and allows contract to start accepting transactions
+        require(running == false, "can't start a bet that's already running");
         startTime = block.timestamp;
         endTime = block.timestamp + _endTime; // the round will end in "_endTime" seconds
         running = true;
+        winningColor = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 6;
     }
     
     function currentTime() public view returns (int256){
-        return int(endTime - block.timestamp);
+        return int(endTime - block.timestamp); // holds time left in a bet
     }
     
-    function checkTimeLeft() public returns (int256){
-        // to be called from the javascript-side every second through setInterval
-        // theoretically it SHOULDN'T require ether if called via using .call()
-        // https://web3js.readthedocs.io/en/1.0/web3-eth.html#eth-call
-        // ^ needs testing w/ front-end though
+    function checkTimeLeft() public returns (int256){ // emits an event if time is out
         int256 time = currentTime();
         if(time < 1){
             emit TimeOut(block.timestamp);
@@ -49,15 +48,30 @@ contract Betting {
     }
     
     function makeBet(uint256 _colorSelected ) public payable returns(bytes32) {
+        bool duplicate = false;
+        uint256 amountOfPlayers = getPlayerAmount();
         require(msg.value >= minimumBet, "did not meet minimum buy-in");
         require(running, "the bet is currently not running");
         require(currentTime() > 0, "time has run out");
+        
         bytes32 betId = keccak256(abi.encodePacked(block.timestamp, msg.sender));
+        for(uint256 i = 0; i < amountOfPlayers; i++){
+            if(players[i] == msg.sender){
+                duplicate = true;
+            }
+        }
+        require(amountOfPlayers < 10 || duplicate, "only 10 players can play in a given round");
+        
+        // set betId, change pool amomunt, and add players to their array
+        
         bets[betId].colorSelected = colorChoices(_colorSelected);
         bets[betId].amountBet = msg.value;
         bets[betId].gambler = msg.sender;
         betIds.push(betId);
         poolAmount = poolAmount + msg.value;
+        if(!duplicate){
+            players[amountOfPlayers] = msg.sender;
+        }
     }
     
     
@@ -65,7 +79,6 @@ contract Betting {
         require(running==true, "you can't end a bet that isn't running");
         running = false; // stop accepting bets
         // get winning colorChoices
-        colorChoices winningColor = colorChoices(winningColor());
         address payable[] memory winners = getWinners(uint256(winningColor));
         uint256 index = 0;
         // gets how many winners there are
@@ -79,15 +92,18 @@ contract Betting {
             uint256 splitWinnings = poolAmount / index; // divide the pool by the amount of winners.
             for(uint256 i=0; i< index; i++){
                 if(winners[i] != address(0)){ //another precaution to make sure no zero addresses are paid, probably redundant 
-                    payWinnings(splitWinnings, winners[i]);
+                    winners[i].transfer(splitWinnings);
                 }
             }
             poolAmount = 0;
         }
         clearBetIds(); // clear bets regardless of outcome
+        clearPlayerAddresses(); // clear player addresses and let other people play
+        
     }
     function getWinners(uint256 _winningColor) public view returns (address payable[] memory) {
-        address payable[] memory winners = new address payable[](10); // limit of 10 participants due to this limitation
+        address payable[] memory winners = new address payable[](10); // limit of 10 participants
+        // because address payable[] must be memory
         uint256 index = 0;
         bool duplicate = false;
         for(uint256 i=0; i<betIds.length; i++) {
@@ -107,35 +123,16 @@ contract Betting {
        }
        return winners;
     }
-   
-    function payWinnings(uint256 funds, address payable gambler) public payable {
-       gambler.transfer(funds);
-    }
-    function winningColor() public returns (uint256) { 
-        uint256 sum = sumOfIds();
-        uint256 color = uint(keccak256(abi.encodePacked(sum)))%6;
-        emit Color(color);
-        return color;
-    }
-    
-    function sumOfIds() public view returns (uint256){
-        uint256 sum = 0;
-        for (uint i=0; i<betIds.length; i++) {
-            sum = sum + uint(betIds[i]);
-        }
-        return sum;
-    }
-    
+    // set everything to 0 when a round is finished
     function clearBetIds() public {
         betIds.length = 0;
     }  
-    
-   // for testing 
+    function clearPlayerAddresses() public {
+        players.length = 0;
+    }
    // basic global variables
    function getBetId() public view returns (bytes32) {
        return betIds[betIds.length -1]; // returns latest bet made
-       // getting the entire array might not be possible due to web3.js limitations
-       // need more research into this though
    }
    function getBetIdLength() public view returns (uint256){
         return betIds.length;
@@ -155,6 +152,9 @@ contract Betting {
    function getPoolAmount() public view returns (uint256){
     return poolAmount;
    }
+   function getWinningColor() public view returns (uint256){
+    return winningColor;
+   }
    // get specific info from betIds
    function getColorSelected(bytes32 id) public view returns (uint256){
         return uint(bets[id].colorSelected);
@@ -164,5 +164,15 @@ contract Betting {
    }
    function getGambler(bytes32 id) public view returns (address payable){
         return bets[id].gambler;
+   }
+   function getPlayerAmount() public view returns (uint256){ // iterate until 10 or 0 address is found, whichever is first
+        uint256 index = 0;
+        while(players[index] != address(0)){
+            index++;
+            if(index==10){
+                break;
+            }
+        }
+       return index;
    }
 }
